187b0da530cd3c2ee26cb65f67b08446260e3429
willDDDD <70461543+willDDDD@users.noreply.github.com>
2021-09-01 09:58:07 -0500

    Update Step1_input_corpus.py

--- Step1_input_corpus.py
+++ Step1_input_corpus.py
@@ -1,12 +1,12 @@
 import json
-target = open("math_corpus.txt", 'w',encoding='utf-8') # file to store the step1 result
+target = open("step1_result.txt", 'w',encoding='utf-8') # file to store the step1 result
 tmp = []
 i = 0
-for line in open(r"C:\Users\Xiuhao Ding\Desktop\arxiv-metadata-oai-snapshot.json",'r'): #arXiv dataset download from https://www.kaggle.com/Cornell-University/arxiv
+for line in open(r"C:\Users\Desktop\arxiv-metadata-oai-snapshot.json",'r'): #arXiv dataset download from https://www.kaggle.com/Cornell-University/arxiv
     tmp.append(json.loads(line))
     
 for i in tmp:
-    if 'math'in i["categories"]:  # "math" can change to other target domain
+    if 'math'in i["categories"]:  # "math" can change to other domain that included in the "categories"
         data= i["abstract"]    # focus on the abtract part
         target.writelines(data+'\n')
         target.writelines(".\n")
