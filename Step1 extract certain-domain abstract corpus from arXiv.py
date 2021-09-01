403755762ba03f807c32678d6efa2b98f6c3b6e7
willDDDD <70461543+willDDDD@users.noreply.github.com>
2021-09-01 09:38:14 -0500

    Add files via upload

---
+++ Step1 extract certain-domain abstract corpus from arXiv.py
@@ -0,0 +1,14 @@
+import json
+target = open("math_corpus.txt", 'w',encoding='utf-8') # file to store the result
+tmp = []
+i = 0
+for line in open(r"C:\Users\Xiuhao Ding\Desktop\arxiv-metadata-oai-snapshot.json",'r'): #arXiv dataset download from https://www.kaggle.com/Cornell-University/arxiv
+    tmp.append(json.loads(line))
+    
+for i in tmp:
+    if 'math'in i["categories"]:  # "math" can change to other domain
+        data= i["abstract"]    # focus on the abtract part
+        target.writelines(data+'\n')
+        target.writelines(".\n")
+target.close()
+
